3c0026d807169e489a487da54a8f084a1721b3e2
jaimejhonle <jaimejhonle@gmail.com>
2018-10-10 17:55:39 -0500

    nuevos cambios

--- assets/angularjs/code/controller.js
+++ assets/angularjs/code/controller.js
@@ -60,6 +60,7 @@ app.controller('CtrlProducts', function($scope, $filter, $location, $http, $rout
             };
             $http.post('Menu/get_all_productos', send).then(function(result) {
                 $scope.totalProductos = result.data.product_count;
+                console.log(result.data.product_count);
             });
             $http.post('Menu/get_productos_x_tipo', send).then(function(result) {
                 $scope.totalProductos = result.data.product_count;
@@ -79,6 +80,7 @@ app.controller('CtrlProducts', function($scope, $filter, $location, $http, $rout
             };
             $http.post('Menu/get_all_productos', send).then(function(result) {
                 $scope.totalProductos = result.data.product_count;
+                console.log(result.data.product_count);
             });
             $http.post('Menu/get_productos_x_tipo', send).then(function(result) {
                 $scope.totalProductos = result.data.product_count;
@@ -98,6 +100,7 @@ app.controller('CtrlProducts', function($scope, $filter, $location, $http, $rout
             };
             $http.post('Menu/get_all_productos', send).then(function(result) {
                 $scope.totalProductos = result.data.product_count;
+                console.log(result.data.product_count);
             });
             $http.post('Menu/get_productos_x_tipo', send).then(function(result) {
                 $scope.totalProductos = result.data.product_count;
